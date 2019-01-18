c184ff7a7a5e3fad1e1c2b90690469b17ff21961
Jack Lund <jackl@geekheads.net>
2019-01-18 09:26:16 -0600

    Refactored profile code

--- src/profile.rs
+++ src/profile.rs
@@ -8,79 +8,113 @@ use std::io::{BufRead, BufReader};
 use std::path;
 use std::str::FromStr;
 
-#[derive(Clone, Debug)]
-pub struct Profile {
-    pub region: Region,
-}
-
-type Config = BTreeMap<String, Profile>;
+type ConfigMap = BTreeMap<String, Profile>;
 
 lazy_static! {
     static ref SECTION_REGEX: Regex = Regex::new(r"^\[([^\]]*)\].*$").unwrap();
     static ref VALUE_REGEX: Regex = Regex::new(r"^\s*([a-zA-Z]*)\s*=\s*(\S*).*$").unwrap();
 }
 
-impl Profile {
-    pub fn get(name: &str) -> Option<Self> {
-        let config = Self::parse_config_file();
-        println!("config: {:?}", config);
+#[derive(Clone, Debug)]
+pub struct Config {
+    map: ConfigMap,
+    profile_name: Option<String>,
+    current_profile: Profile,
+}
 
-        config.get(name).map(|value| (*value).clone())
+impl Default for Config {
+    fn default() -> Self {
+        let mut map = ConfigMap::new();
+        map.insert("default".to_string(), Profile::default());
+
+        Config {
+            map,
+            profile_name: None,
+            current_profile: Profile::default(),
+        }
+    }
+}
+
+impl Config {
+    fn get() -> Self {
+        let mut config = Config::default();
+        config.parse_config_file(&Config::get_config_file_path());
+
+        config
     }
 
-    fn parse_config_file() -> Config {
-        let file_path = Self::get_config_file_path();
-        let mut config: Config = Config::new();
-        let mut current_profile: Profile = Profile::default();
-        if !file_path.exists() {
-            config.insert("default".to_string(), current_profile);
-            return config;
-        }
-        println!("Opening {:?}", file_path);
-        let file = File::open(file_path.clone())
-            .unwrap_or_else(|_| panic!("Error opening config file {:?}", file_path));
-        let mut profile_name: Option<String> = None;
-        for line_or_error in BufReader::new(file).lines() {
-            match line_or_error {
-                Err(error) => panic!("Error reading config file: {}", error),
-                Ok(line) => {
-                    println!("Reading line '{}'", line);
-                    if SECTION_REGEX.is_match(&line) {
-                        if profile_name.is_some() {
-                            config.insert(profile_name.unwrap(), current_profile);
-                            current_profile = Profile::default();
-                        }
-                        profile_name = Some(
-                            SECTION_REGEX
-                                .captures(&line)
-                                .unwrap()
-                                .get(1)
-                                .unwrap()
-                                .as_str()
-                                .to_string(),
-                        );
-                    } else if VALUE_REGEX.is_match(&line) {
-                        let captures = VALUE_REGEX.captures(&line).unwrap();
-                        if profile_name.is_none() {
-                            println!("Got entry in config outside profile");
-                        } else {
-                            let key = captures.get(1).unwrap().as_str();
-                            if key == "region" {
-                                let region_name = captures.get(2).unwrap().as_str();
-                                println!("Got region {}", region_name);
-                                current_profile.region = Region::from_str(region_name)
-                                    .expect("Error parsing AWS config file");
-                            }
-                        }
-                    }
-                }
-            };
-        }
+    fn add_profile(&mut self, name: &str, profile: Profile) {
+        self.map.insert(name.to_string(), profile);
+    }
 
-        if profile_name.is_some() {
-            config.insert(profile_name.unwrap(), current_profile);
+    fn get_profile(&self, name: &str) -> Option<&Profile> {
+        let profile_name = if name == "default" {
+            name.to_string()
+        } else {
+            format!("profile {}", name)
+        };
+        self.map.get(&profile_name)
+    }
+
+    fn parse_config_file(&mut self, path: &path::PathBuf) {
+        if path.exists() {
+            let file = File::open(path.clone())
+                .unwrap_or_else(|_| panic!("Error opening config file {:?}", path));
+            for line_or_error in BufReader::new(file).lines() {
+                match line_or_error {
+                    Err(error) => panic!("Error reading config file: {}", error),
+                    Ok(line) => {
+                        self.parse_line(&line);
+                    }
+                };
+            }
+
+            if self.profile_name.is_some() {
+                let name = self.clone().profile_name.unwrap();
+                let profile = self.current_profile.clone();
+                self.add_profile(&name, profile);
+            }
         }
-        config
+    }
+
+    fn parse_line(&mut self, line: &str) {
+        if SECTION_REGEX.is_match(&line) {
+            self.set_section_name(
+                SECTION_REGEX
+                    .captures(&line)
+                    .unwrap()
+                    .get(1)
+                    .unwrap()
+                    .as_str(),
+            );
+        } else if VALUE_REGEX.is_match(&line) {
+            let captures = VALUE_REGEX.captures(&line).unwrap();
+            self.set_value(
+                captures.get(1).unwrap().as_str(),
+                captures.get(2).unwrap().as_str(),
+            );
+        }
+    }
+
+    fn set_section_name(&mut self, section_name: &str) {
+        if self.profile_name.is_some() {
+            let config = self.clone();
+            let profile_name = config.profile_name.unwrap().clone();
+            let profile = config.current_profile.clone();
+            self.add_profile(&profile_name, profile);
+            self.current_profile = Profile::default();
+        }
+        self.profile_name = Some(section_name.to_string());
+    }
+
+    fn set_value(&mut self, key: &str, value: &str) {
+        if self.profile_name.is_none() {
+            println!("Got entry in config outside profile");
+        } else if key == "region" {
+            let region_name = value;
+            self.current_profile.region =
+                Region::from_str(region_name).expect("Error parsing AWS config file");
+        }
     }
 
     fn get_config_file_path() -> path::PathBuf {
@@ -99,10 +133,23 @@ impl Profile {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct Profile {
+    pub region: Region,
+}
+
 impl Default for Profile {
     fn default() -> Self {
         Profile {
             region: Region::default(),
         }
+    }
+}
+
+impl Profile {
+    pub fn get(name: &str) -> Option<Profile> {
+        let config = Config::get();
+
+        config.get_profile(name).cloned()
     }
 }
