c3cd637f69395e99116847b1ea6679922c03b2fd
Jack Lund <jackl@geekheads.net>
2022-11-04 19:29:04 -0500

    Remove terraform stuff

--- terraform/providers.tf
+++
@@ -1,5 +0,0 @@
-# Default provider
-provider "aws" {
-  region  = "us-east-2"
-  version = "~> 2.0"
-}
--- terraform/remote_state.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "s3_package_repo_state" {
-  backend = "s3"
-  config = {
-    bucket = "jack-terraform-tfstate"
-    region = "us-east-2"
-    key    = "s3-package-repo.tfstate"
-  }
-}
